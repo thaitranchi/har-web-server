0dabcf9785269850b82a9297c315bdeab0e43cb0
Trần Chí Thái <31505274+thaitranchi@users.noreply.github.com>
2026-04-14 21:58:14 +0700

    Refactor ReportForm and add ConnectionForm
    Updated ReportForm to inherit from forms.ModelForm and removed 'user' field. Added ConnectionForm for user authentication.

--- har/report/forms.py
+++ har/report/forms.py
@@ -1,8 +1,13 @@
-from django.forms import ModelForm
+from django import models
+from django import forms
 from .models import Report
+from django.contrib.auth.models import User
 
-# create report form
-class ReportForm(ModelForm):
+class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ['user', 'name', 'latitude', 'longitude', 'altitude', 'accuracy']
+        fields = ['name', 'latitude', 'longitude', 'altitude', 'accuracy']
+
+class ConnectionForm(forms.Form):
+    email = forms.EmailField(label="Email Address")
+    password = forms.CharField(widget=forms.PasswordInput)
