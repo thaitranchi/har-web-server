487d64c407000b4d46c05f35bbc0c8cc5d5ce0a4
Trần Chí Thái <31505274+thaitranchi@users.noreply.github.com>
2026-04-14 21:57:56 +0700

    Refactor Report model fields and types

--- har/report/models.py
+++ har/report/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Report model
 class Report(models.Model):
+    name = models.CharField(max_length=255)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.TextField()
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    altitude = models.FloatField()
-    accuracy = models.FloatField(null=True, blank=True)
-    creation_time = models.DateTimeField(auto_now=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    altitude = models.DecimalField(max_digits=9, decimal_places=2)
+    accuracy = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
