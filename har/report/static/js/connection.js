0d030015c309b48231d6aa8495d4f1d0e8ed7ed4
thaitranchi <chithai1999@gmail.com>
2026-04-17 17:44:52 +0700

    Fix templpates

--- har/report/static/js/connection.js
+++ har/report/static/js/connection.js
@@ -6,10 +6,28 @@ function onClickEyeIcon() {
 
     if (password.type === "password") {
         password.type = "text";
-        eyebox.style.opacity = "0.5"; // Dim it or change icon
+        eyebox.style.opacity = "0.5"; // Dim it slightly to indicate state
+        if (eyeIcon) {
+            // When password becomes visible, show the plain eye (no slash)
+            if (eyeIcon.src && eyeIcon.src.indexOf('ic_eye_slash.png') !== -1) {
+                eyeIcon.src = eyeIcon.src.replace('ic_eye_slash.png', 'ic_eye.png');
+            } else if (eyeIcon.getAttribute('data-eye')) {
+                eyeIcon.src = eyeIcon.getAttribute('data-eye');
+            }
+            eyeIcon.setAttribute('aria-pressed','true');
+        }
     } else {
         password.type = "password";
         eyebox.style.opacity = "1"; // Standard full opacity
+        if (eyeIcon) {
+            // When password is hidden, show the slashed eye
+            if (eyeIcon.src && eyeIcon.src.indexOf('ic_eye.png') !== -1) {
+                eyeIcon.src = eyeIcon.src.replace('ic_eye.png', 'ic_eye_slash.png');
+            } else if (eyeIcon.getAttribute('data-eye-slash')) {
+                eyeIcon.src = eyeIcon.getAttribute('data-eye-slash');
+            }
+            eyeIcon.setAttribute('aria-pressed','false');
+        }
     }
 }
 
